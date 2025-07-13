0b4d30295e0427a01c954a41f23a493bd0f4814c
keontapeat <keontapeat@gmail.com>
2025-07-13 09:53:04 -0400

    Initial Commit

---
+++ GOLDEX AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GOLDEX AI
+//
+//  Created by Keonta  on 7/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GOLDEX AI/GOLDEX_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOLDEX_AIApp.swift
+//  GOLDEX AI
+//
+//  Created by Keonta  on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GOLDEX_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
